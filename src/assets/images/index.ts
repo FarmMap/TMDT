90b2cbedd2c41e92423ce7df6e7ca3134563370f
troliaoluatsu <thienan041803@gmail.com>
2023-10-31 13:31:13 +0700

    Default Layout UI

---
+++ src/assets/images/index.ts
@@ -0,0 +1,6 @@
+const images = {
+  yecaubaogia: require("../images/yeucaubaogia.png"),
+  traiCayTuoi: require("../images/traiCayTuoi.png"),
+};
+
+export default images;
--- src/presentations/pages/home/HomePage.tsx
+++ src/presentations/pages/home/HomePage.tsx
@@ -1,10 +1,66 @@
 // External files
+import Grid from "@mui/material/Grid";
 import React from "react";
+import DefaultLayOut from "../../components/defaultLayOut/DefaultLayOut";
+import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
 // Internal files
 // Styles
+import classNames from "classnames/bind";
+import styles from "./Home.module.scss";
+import images from "../../../assets/images";
+
+const cx = classNames.bind(styles);
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <DefaultLayOut>
+      <Grid container className={cx("wrapper")}>
+        <Grid item lg={3}>
+          <Grid className={cx("heading-sidebar")}> Gợi ý cho bạn</Grid>
+          <Grid className={cx("sidebar-item")}>
+            <img src={images.traiCayTuoi} alt="ITFS sidebar" />
+            <span>
+              Trái cây
+              <KeyboardArrowRightRoundedIcon />
+            </span>
+          </Grid>
+
+          <Grid className={cx("sidebar-item")}>
+            <img src={images.traiCayTuoi} alt="ITFS sidebar" />
+            <span>
+              Trái cây
+              <KeyboardArrowRightRoundedIcon />
+            </span>
+          </Grid>
+
+          <Grid className={cx("sidebar-item")}>
+            <img src={images.traiCayTuoi} alt="ITFS sidebar" />
+            <span>
+              Trái cây
+              <KeyboardArrowRightRoundedIcon />
+            </span>
+          </Grid>
+
+          <Grid className={cx("sidebar-item")}>
+            <img src={images.traiCayTuoi} alt="ITFS sidebar" />
+            <span>
+              Trái cây
+              <KeyboardArrowRightRoundedIcon />
+            </span>
+          </Grid>
+
+          <Grid className={cx("sidebar-item")}>
+            <img src={images.traiCayTuoi} alt="ITFS sidebar" />
+            <span>
+              Trái cây
+              <KeyboardArrowRightRoundedIcon />
+            </span>
+          </Grid>
+        </Grid>
+        <Grid item lg={9}></Grid>
+      </Grid>
+    </DefaultLayOut>
+  );
 };
 
 export default HomePage;
